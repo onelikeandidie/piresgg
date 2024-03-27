011eb9fc91a633eb9c26e527fe04893dc36f2c5e
onlikeandidie <onelikeandidie@proton.me>
2024-03-27 16:29:31 +0000

    Added: Tailwind and Esbuild with minification

--- server/build.rs
+++ server/build.rs
@@ -2,15 +2,49 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=templates/");
-    // Run tailwind for the templates
-    Command::new("node_modules/.bin/tailwindcss")
-        .args(["-i", "frontend/tailwind.css", "-o", "public/main.css"])
-        .status()
-        .unwrap();
+    // Run tailwind for the templates for debug
+    #[cfg(debug_assertions)]
+    {
+        Command::new("node_modules/.bin/tailwindcss")
+            .args(["-i", "frontend/tailwind.css", "-o", "public/main.css"])
+            .status()
+            .unwrap();
+    }
+    // Run tailwind for the templates for release
+    #[cfg(not(debug_assertions))]
+    {
+        Command::new("node_modules/.bin/tailwindcss")
+            .args([
+                "-i",
+                "frontend/tailwind.css",
+                "-o",
+                "public/main.css",
+                "--minify",
+            ])
+            .status()
+            .unwrap();
+    }
 
     println!("cargo:rerun-if-changed=frontend/main.ts");
-    Command::new("node_modules/.bin/esbuild")
-        .args(["frontend/main.ts", "--bundle", "--outfile=public/main.js"])
-        .status()
-        .unwrap();
+    // Run esbuild for the frontend for debug
+    #[cfg(debug_assertions)]
+    {
+        Command::new("node_modules/.bin/esbuild")
+            .args(["frontend/main.ts", "--bundle", "--sourcemap", "--outfile=public/main.js"])
+            .status()
+            .unwrap();
+    }
+    // Run esbuild for the frontend for release
+    #[cfg(not(debug_assertions))]
+    {
+        Command::new("node_modules/.bin/esbuild")
+            .args([
+                "frontend/main.ts",
+                "--bundle",
+                "--minify",
+                "--outfile=public/main.js",
+            ])
+            .status()
+            .unwrap();
+    }
 }
